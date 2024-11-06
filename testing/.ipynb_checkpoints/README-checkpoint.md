d9e17120e8d4008edd95438abaa90226ee8dde34
William Loving <wloving77@gmail.com>
2024-11-06 03:40:26 +0000

    deleting cache

--- testing/.ipynb_checkpoints/README-checkpoint.md
+++
@@ -1,3 +0,0 @@
-# Directory for Storing Different Telemetry Calculations. 
-
-Currently just packet_counting and an example
--- testing/packet_counting_p4/.ipynb_checkpoints/h1_packet_counting-checkpoint.sh
+++
@@ -1,2 +0,0 @@
-sudo python3 host_scripts/packet_counter.py > tmp/h1_packet_counting_output.txt &
-sudo python3 host_scripts/packet_counting_timed.py > tmp/h1_packet_counting_latency.txt &
