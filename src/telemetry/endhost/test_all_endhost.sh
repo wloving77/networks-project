359ebd8942cedb1dbbeb60f34b4c594f65290b66
William Loving <wloving77@gmail.com>
2024-12-15 16:46:23 -0500

    on a roll

--- src/telemetry/endhost/test_all_endhost.sh
+++ src/telemetry/endhost/test_all_endhost.sh
@@ -1,6 +1,6 @@
 for i in {1..15}
 do
-    python3 ../rtt/endhost_scripts/rtt.py >> ../tmp/rtt_execution_time.txt 
-    python3 ../packet_counting/endhost_scripts/packet_counting.py >> ../tmp/packet_counting_execution_time.txt 
-    python3 ../jitter/endhost_scripts/jitter.py >> ../tmp/jitter_execution_time.txt 
+    python3 ../rtt/endhost_scripts/rtt.py >> ../results/rtt_execution_time.txt 
+    python3 ../packet_counting/endhost_scripts/packet_counting.py >> ../results/packet_counting_execution_time.txt 
+    python3 ../jitter/endhost_scripts/jitter.py >> ../results/jitter_execution_time.txt 
 done
